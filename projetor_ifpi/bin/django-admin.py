6120da2c5bb319977213d28b639cb08e6211d5c0
Guilherme2020 <guilhermerodrigues123477@gmail.com>
2017-08-17 22:53:48 -0300

    configuracao do ambiente

---
+++ projetor_ifpi/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/grodrigues/Documentos/ProjetosWeb/projetor/projetor_ifpi/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
